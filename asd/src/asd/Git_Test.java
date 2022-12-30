2583d0a64dbdde83db781ca05cf9115aa5f56cf8
ParkGiBeom97 <84974942+ParkGiBeom97@users.noreply.github.com>
2022-12-30 16:20:03 +0900

    Delete asd directory

--- asd/src/asd/Git_Test.java
+++
@@ -1,7 +0,0 @@
-package asd;
-
-public class sdasdasd {
-	public static void main(String[] args) {
-		System.out.println("asdasdasd");
-	}
-}
--- asd/src/asd/asdasdas.java
+++
@@ -1,7 +0,0 @@
-package asd;
-
-public class asdasdas {
-	public static void main(String[] args) {
-		System.out.println("asdasdasd");
-	}
-}
